c4524baa6227b670690f9d30ff1c1026b38045fd
kurenc0va <polinakurenkova2004@gmail.com>
2023-11-07 15:55:52 +0400

    in process

---
+++ lr4/4.1.sql
@@ -0,0 +1,9 @@
+/* Добавить новый объект (‘spa’), стоимость создания (initialoutlay) = 
+100000, стоимость обслуживания (monthlymaintenance) = 800, стоимость 
+аренды гостем = 30, членом клуба = 20.*/
+
+USE cd;
+INSERT INTO facilities (facid, facility, membercost, guestcost, initialoutlay, monthlymaintenance)
+/*Добавляем новый объект в таблицу*/
+VALUES (9, 'spa', 20, 30, 100000, 800);
+SELECT * FROM facilities;
