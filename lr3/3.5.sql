8cc1c07c93f217437c8124979cbb6203317733b7
kurenc0va <polinakurenkova2004@gmail.com>
2023-11-15 13:50:46 +0400

    done

--- lr3/3.5.sql
+++ lr3/3.5.sql
@@ -1,7 +1,8 @@
+-- Active: 1696918614352@@localhost@3306@cd
 /*Выберите ФИО (== имя + фамилия) всех, кто покупал корты 1 и 2.*/
 
 USE cd;
 SELECT DISTINCT firstname, surname FROM members
 JOIN bookings ON members.memid = bookings.memid
 JOIN facilities ON facilities.facid = bookings.facid
-WHERE facilities.facility LIKE 'Tennis court 1' OR facilities.facility LIKE 'Tennis court 2';
+WHERE facilities.facility LIKE '%Court 1%' OR facilities.facility LIKE '%Court 2%';
