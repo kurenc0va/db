ff37182827c52c1688281ab370966bd8f6a51833
kurenc0va <polinakurenkova2004@gmail.com>
2023-11-21 11:16:30 +0400

    done

--- lr5/5.5.sql
+++ lr5/5.5.sql
@@ -1,2 +1,6 @@
+-- Active: 1696918614352@@localhost@3306@cd
 /* Рассчитайте количество аренд каждого из объектов клуба за 
 сентябрь 2012 года.*/
+USE cd;
+SELECT facility, sum(b.slots) AS 'Количество аренд' FROM facilities fac
+INNER JOIN bookings AS b ON b.facid = fac.facid GROUP BY fac.facid;
