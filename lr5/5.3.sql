efc15bfb7a033fafca9f591623793242ee5bbca5
kurenc0va <polinakurenkova2004@gmail.com>
2023-11-27 12:18:22 +0400

    done

--- lr5/5.3.sql
+++ lr5/5.3.sql
@@ -4,9 +4,6 @@
 рекомендаций.*/
 USE cd;
 
-/*Выбираем идентификатор участника, имя, фамилию и количество рекомендаций для каждого участника*/
-USE cd;
-
 SELECT CONCAT(m1.surname, ' ', m1.firstname) AS 'ФИО', COUNT(m2.memid) AS 'Количество рекомендаций'
   FROM members m1
   JOIN members AS m2 ON m2.recommendedby = m1.memid
