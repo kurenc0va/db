55067f62a15245f20e138770392ec593171add7a
kurenc0va <polinakurenkova2004@gmail.com>
2023-11-28 10:36:44 +0400

    done

--- lr5/5.4.sql
+++ lr5/5.4.sql
@@ -1,15 +1,11 @@
 -- Active: 1696918614352@@localhost@3306@cd
 /* Рассчитайте количество аренд каждого из объектов клуба.*/
 USE cd;
-
 /* Выбираем идентификатор объекта, название объекта и количество бронированных слотов для каждого объекта*/
 SELECT
     fac.facid AS Facid,
     fac.facility AS Facility,
     SUM(b.slots) AS reccommend
-FROM
-    facilities fac
-JOIN
-    bookings b ON b.facid = fac.facid
-GROUP BY
-    fac.facid, fac.facility;
+FROM facilities fac
+JOIN bookings b ON b.facid = fac.facid
+GROUP BY fac.facid, fac.facility;
