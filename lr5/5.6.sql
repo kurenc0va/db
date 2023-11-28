5dfdecf75219df6e1a98e373296747010b59b380
kurenc0va <polinakurenkova2004@gmail.com>
2023-11-28 10:22:14 +0400

    done

--- lr5/5.6.sql
+++ lr5/5.6.sql
@@ -1,2 +1,10 @@
+-- Active: 1696918614352@@localhost@3306@cd
 /*Составьте список общего количества бронирований на каждый 
 объект в месяц в 2012 году.*/
+USE cd;
+
+SELECT f.facility, MONTH(b.starttime) AS 'Месяц', SUM(b.slots) AS 'Количество бронирований' 
+  FROM facilities f
+    JOIN bookings b ON f.facid = b.facid
+  WHERE YEAR(b.starttime) = "2012"
+  GROUP BY f.facility, MONTH(b.starttime);
