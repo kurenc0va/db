15e59c39eb454b1438f5135ae7df5df4cd97340f
kurenc0va <polinakurenkova2004@gmail.com>
2023-11-28 17:22:04 +0400

    done

--- lr5/5.15.sql
+++ lr5/5.15.sql
@@ -1,4 +1,16 @@
+-- Active: 1696918614352@@localhost@3306@cd
 /*Составьте список участников (включая гостей) вместе с количеством часов, которые они забронировали для объекта,  
 округленным до ближайших десяти часов. 
 Ранжируйте их по этой округленной цифре, получая в результате имя, фамилию, округленные часы и звание (== ранг). 
 Сортировка по званию (== рангу), фамилии и имени.*/
+
+USE cd;
+
+SELECT b.memid, m.surname, m.firstname,
+       ROUND(SUM(slots) / 2, -1) AS fachours,
+       RANK() OVER(ORDER BY ROUND(SUM(slots) / 2, -1)) AS ranking,
+       DENSE_RANK() OVER(ORDER BY ROUND(SUM(slots) / 2, -1)) AS dranking
+  FROM bookings as b
+    LEFT JOIN members AS m ON b.memid = m.memid
+  GROUP BY b.memid
+  ORDER BY ranking, surname, firstname;
--- lr5/5.16.sql
+++ lr5/5.16.sql
@@ -1,3 +1,8 @@
 /*Составьте список трех крупнейших объектов, приносящих доход (включая связи).
  Вывод названия и ранга объекта, отсортированный по рангу и названию объекта.
 Примечание: используйте функцию ранжирования*/
+USE cd;
+SELECT f.facility, RANK() OVER (ORDER BY SUM(IF(b.memid = 0, f.guestcost * b.slots, f.membercost * b.slots)) DESC) as rankk
+FROM facilities f
+JOIN bookings b ON f.facid = b.facid GROUP BY f.facility ORDER BY rankk, f.facility
+LIMIT 3;
