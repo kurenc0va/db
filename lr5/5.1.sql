2185fe4511fc8686ce14fc98224afb7d9bfe2a0e
kurenc0va <polinakurenkova2004@gmail.com>
2023-12-04 17:17:49 +0400

    done

--- lr5/5.1.sql
+++ lr5/5.1.sql
@@ -1,4 +1,4 @@
  /*Рассчитайте количество зарегистрированных объектов в теннисном клубе.*/
 USE cd;
 /*Выбираем количество записей в столбце "facility" таблицы "facilities"*/
-SELECT COUNT(facility) AS "Всего зарегестрированных объектов" FROM facilities;
+EXPLAIN SELECT COUNT(facid) AS "Всего зарегестрированных объектов" FROM facilities
