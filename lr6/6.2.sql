78e11c3387ac04ab1c739de9db0f8c7917108e2d
kurenc0va <polinakurenkova2004@gmail.com>
2023-12-05 22:04:38 +0400

    done

--- lr6/6.2.sql
+++ lr6/6.2.sql
@@ -1,5 +1,4 @@
-/*Выберите количество аренд объектов клуба в каждый месяц.*/
-USE cd;
+/*Выберите количество аренд объектов клуба в каждый месяц.*/USE cd;USE cd;
 SELECT YEAR(starttime) AS Год, MONTH(starttime) AS Месяц, facility AS 'Название объекта', COUNT(*) AS 'Количество аренд'
 FROM bookings AS book
 JOIN facilities AS fac ON book.facid = fac.facid
