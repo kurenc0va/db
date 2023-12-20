71a70671ca7a5f0002985d16475e16fcbe823cea
kurenc0va <polinakurenkova2004@gmail.com>
2023-12-20 12:38:04 +0400

    done

--- lr7/7.1.sql
+++ lr7/7.1.sql
@@ -1,1 +1,13 @@
-USE cd 
+USE cd;
+DELIMITER //
+DROP FUNCTION IF EXISTS slot_cost//
+CREATE FUNCTION slot_cost(memid int, facid int, slots int) RETURNS DECIMAL (5, 2)
+DETERMINISTIC
+BEGIN 
+DECLARE cost int;
+SET cost = (SELECT IF (memid = 0, guestcost, membercost)*slots FROM facilities WHERE facid = facilities.facid);
+RETURN cost;
+END//
+DELIMITER ;
+SELECT slot_cost(memid, facid, slots)
+FROM bookings;
